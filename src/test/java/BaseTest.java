a336bdfb7876d3de451e4c4769afcf8ea652788a
Alex G <ulledar@ua.fm>
2021-03-16 18:36:43 +0200

    BasicTest upd

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,5 +1,6 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selenide.open;
 
@@ -7,9 +8,15 @@ public class BaseTest {
 
     @BeforeEach
     void start() {
-        Configuration.startMaximized = true;
+        Configuration.remote = "http://localhost:4444/wd/hub";
+        Configuration.browser = "chrome";
+        Configuration.startMaximized = false;
+        DesiredCapabilities dc = new DesiredCapabilities();
+        dc.setCapability("enableVNC", true);
+        Configuration.browserCapabilities = dc;
         Configuration.browser = "chrome";
         Configuration.baseUrl = "https://ek.ua/";
+        Configuration.driverManagerEnabled = false;
     }
 
 }
