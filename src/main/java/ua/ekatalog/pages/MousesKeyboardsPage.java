eecf67e01c78b8fc4e981ad6ce14e436a73b32a2
ulledar <ulledar@ua.fm>
2021-04-05 13:22:46 +0300

    2 new PageObject classes

--- src/main/java/ua/ekatalog/pages/MousesKeyboardsPage.java
+++ src/main/java/ua/ekatalog/pages/MousesKeyboardsPage.java
@@ -8,17 +8,17 @@ import static com.codeborne.selenide.Selenide.$;
 public class MousesKeyboardsPage {
 
     private final By
-            mouse = By.cssSelector("a[href='/k267.htm']"),
-            keyboard = By.cssSelector("a[href='/k179.htm']");
+            mouses = By.cssSelector("a[href='/k267.htm']"),
+            keyboards = By.cssSelector("a[href='/k179.htm']");
 
     @Step("chose mouses")
     public void mouseChose() {
-        $(mouse).click();
+        $(mouses).click();
     }
 
     @Step("chose keyboards")
     public void keyboardChose() {
-        $(keyboard).click();
+        $(keyboards).click();
     }
 
 }
